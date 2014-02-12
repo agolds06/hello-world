746635c44f2a1cb2d55f12686b08ff2638bfe0ce
Alex Goldschmidt <alex.goldschmidt@tufts.edu>
2014-02-12 18:03:14 -0500

    Lab 2 again

---
+++ comp23-agoldschmidt/python/ip_addresses.py
@@ -0,0 +1,5 @@
+for i in range(0,256):
+    for j in range(0,256):
+        for k in range(0,256):
+            for l in range(0,256):
+                print(str(i) + "-" + str(j) + "-" + str(k) + "-" + str(l))
---
+++ comp23-agoldschmidt/python/reverselist.py
@@ -0,0 +1,3 @@
+words = ["dog", "cat", "fish"];
+for i in reversed(words):
+    print i
